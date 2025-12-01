6fb4dacec055222b201c6664dab19827c1a2ad28
hunterckm01 <huner6894@gmail.com>
2025-12-01 19:30:37 +0530

    Added Captain Middleware to Ride Route

--- Ride/middlewares/auth.middleware.js
+++ Ride/middlewares/auth.middleware.js
@@ -39,3 +39,29 @@ module.exports.userAuth = async(req, res, next) => {
         });
     }
 }
+
+module.exports.captainAuth = async(req, res, next) => {
+    try{
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const response = await axios.get(`${BASE_URL}/captain/profile`, {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+
+        const captain = response.data;
+
+        if(!captain)
+            return res.status(401).json({
+        message: "Unauthorized Access"})
+
+        req.captain = captain;  
+        next();
+    }
+    catch(err){
+        res.status(200).json({
+            message: err.message
+        })
+    }
+}
